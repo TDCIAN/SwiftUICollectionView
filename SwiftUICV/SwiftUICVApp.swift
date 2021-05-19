ef7e5af2e8a97385b38bf860333cd249be7b9f37
Jeongmin <tdcian71@gmail.com>
2021-05-19 19:48:26 +0900

    Initial Commit

---
+++ SwiftUICV/SwiftUICVApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICVApp.swift
+//  SwiftUICV
+//
+//  Created by APPLE on 2021/05/19.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
